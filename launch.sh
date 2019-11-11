8e7a7998c495252e5948421483fb7b96e68357ca
Andrea F. Daniele <afdaniele@ttic.edu>
2019-11-11 17:37:46 -0600

    removed config_filename from launch.sh; split config path string in launchfile

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch foscam_ros foscam_ros.launch camera_name:=foscam_r2 param_file_name:=ttic
+roslaunch foscam_ros foscam_ros.launch camera_name:=foscam_r2
